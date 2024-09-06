4acf8b0c94bc472082f1cd7f19cff977b0c2da18
ReadyBlast <acarnaval96@gmail.com>
2024-09-06 17:08:36 +0400

    Created basic components for homepage. Added Zustand. Homepage basically typesetted with mocked data

---
+++ components/shared/top-bar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { Container } from './container';
+import { SortPopup } from './sort-popup';
+import { Categories } from './categories';
+
+interface TopBarProps {
+  className?: string;
+}
+
+export const TopBar: React.FC<TopBarProps> = ({ className }) => {
+  return (
+    <div
+      className={cn(
+        'sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10',
+        className,
+      )}
+    >
+      <Container className="flex items-center justify-between">
+        <Categories />
+        <SortPopup />
+      </Container>
+    </div>
+  );
+};
